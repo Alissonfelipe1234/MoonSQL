3e9c1fbe176759bbad12b620966aea6875ca2b88
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2018-11-12 18:34:52 -0200

    add desenho

---
+++ FlowModel/Desenho.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowModel
+{
+    interface Desenho
+    {
+        void SeDesenhe(System.Windows.Forms.Form d);
+        string QuemSou();
+        void Propriedades(System.Windows.Forms.Panel p);
+        bool GetArea(int x, int y);
+    }
+}
