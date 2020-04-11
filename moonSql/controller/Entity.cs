6b48bb221d1fa49c8c934b803b1fdd6b14da2487
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-11 15:28:42 -0300

    Entity object
    first implemation of Entity drawable object

---
+++ moonSql/controller/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace moonSql.controller
+{
+    class Entity : Drawable
+    {
+        private int x;
+        private int y;
+
+        private List<Drawable> childs;
+
+        public void DrawIt(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+        public int GetX()
+        {
+            return this.x;
+        }
+        public int GetY()
+        {
+            return this.y;
+        }
+        public bool IsThere(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetX(int newX)
+        {
+            this.x = newX;
+        }
+        public void SetY(int newY)
+        {
+            this.x = newY;
+        }
+    }
+}
