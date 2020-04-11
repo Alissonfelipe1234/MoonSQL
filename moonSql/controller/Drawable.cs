885ac4364c657ff2ef96131decdd2e4ddfcffa1e
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2020-04-11 15:27:39 -0300

    Drawable interface
    this is a interface for all drawable objects

---
+++ moonSql/controller/Drawable.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace moonSql.controller
+{
+    interface Drawable
+    {
+        int GetX();
+        int GetY();
+        bool IsThere(int x, int y);
+        void SetX(int newX);
+        void SetY(int newY);
+        void DrawIt(Graphics g);
+    }
+}
